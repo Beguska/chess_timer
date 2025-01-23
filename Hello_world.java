8367370408d749a7e6eea851a8e5654e646f884c
Beguska <blackfrog20000@gmail.com>
2025-01-23 17:17:16 +0100

    Second commit

--- Hello_world.java
+++ Hello_world.java
@@ -1,7 +1,7 @@
 public class Hello_world {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Chess Timner sdfsdf we gooooo");
+        System.out.println("Chess Timnsdfsdffsdf we gooooo");
 
     }
 }
