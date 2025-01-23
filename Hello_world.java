7656374b3b2db928619495a7e5b224287ba5b4d4
Beguska <blackfrog20000@gmail.com>
2025-01-23 17:16:30 +0100

    Second commit

--- Hello_world.java
+++ Hello_world.java
@@ -1,5 +1,7 @@
 public class Hello_world {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Chess Timner sdfsdf we gooooo");
+
     }
 }
